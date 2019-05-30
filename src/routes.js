dadb6ffc4b0030ae23085e4f0c7a5a1ca5fdae27
Diego de Souza Vale Moreno <diego.moreno@linx.com.br>
2019-05-30 13:56:32 -0300

    Correção na utilização do styles global.

--- src/routes.js
+++ src/routes.js
@@ -6,23 +6,14 @@ import {
 import React, { Component } from 'react';
 import Welcome from '~/pages/Welcome';
 import Appointment from '~/pages/Appointment';
-import HeaderRight from '~/components/HeaderRight';
+// import HeaderRight from '~/components/HeaderRight';
 
-import { colors, metrics } from './styles';
+// import { colors, metrics } from './styles';
 
-class SettingsScreen extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
 const Routes = (userLogged = false) => createAppContainer(
   createSwitchNavigator({
     Welcome,
-    SettingsScreen,
+    Appointment,
   }),
 );
 
